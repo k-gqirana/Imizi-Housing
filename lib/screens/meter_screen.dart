b060977ce5675fca250d61856aa5c4ccccd47228
k-gqirana <s216057361@mandela.ac.za>
2023-09-26 00:54:02 +0200

    ScrollView, Home and Submit buttons on Meter Screen

--- lib/screens/meter_screen.dart
+++ lib/screens/meter_screen.dart
@@ -50,27 +50,23 @@ class Units {
   final int unitNumber;
   final int propertyId;
   final int payPropUnitId;
-  final String createDate;
-  final bool active;
 
-  Units(
-      {required this.unitId,
-      required this.blockId,
-      required this.unitNumber,
-      required this.propertyId,
-      required this.payPropUnitId,
-      required this.createDate,
-      required this.active});
+  Units({
+    required this.unitId,
+    required this.blockId,
+    required this.unitNumber,
+    required this.propertyId,
+    required this.payPropUnitId,
+  });
 
   factory Units.fromJson(Map<String, dynamic> json) {
     return Units(
-        unitId: json['unitId'],
-        blockId: json['blockId'],
-        unitNumber: json['unitNumber'],
-        propertyId: json['propertyId'],
-        payPropUnitId: json['payPropUnitId'],
-        createDate: json['createDate'],
-        active: json['active']);
+      unitId: json['unitId'],
+      blockId: json['blockId'],
+      unitNumber: json['unitNumber'],
+      propertyId: json['propertyId'],
+      payPropUnitId: json['payPropUnitId'],
+    );
   }
 }
 
@@ -87,7 +83,10 @@ class _MeterScreenState extends State<MeterScreen> {
   late Future<List<Blocks>> futureBlocks;
   late Future<List<Units>> futureUnits;
   Blocks? selectedBlock; // Define selectedBlock as nullable
-  int? selectedBlockID; // To fetch the units of the selected block
+  late int selectedBlockID; // To fetch the units of the selected block
+
+  //Scroll Controller
+  final ScrollController _firstController = ScrollController();
 
   @override
   void initState() {
@@ -102,7 +101,7 @@ class _MeterScreenState extends State<MeterScreen> {
         setState(() {
           selectedBlock = blocks.first;
 
-          selectedBlockID = selectedBlock?.blockId;
+          selectedBlockID = selectedBlock!.blockId;
           // call method to fetch units when block is clicked
           futureUnits = fetchUnits();
         });
@@ -131,26 +130,21 @@ class _MeterScreenState extends State<MeterScreen> {
 
   //Bring List of Units when a block is pressed
   Future<List<Units>> fetchUnits() async {
-    final Map<String, dynamic> queryParams = {};
+    //Base URL
+    final baseUrl =
+        Uri.parse('https://imiziapi.codeflux.co.za/api/Unit/Search');
 
-    if (selectedBlockID != null) {
-      queryParams['BlockId'] = 1; // Include BlockId if it's not null
-    }
-
-    final uri =
-        Uri.https('imiziapi.codeflux.co.za', 'api/Unit/Search', queryParams);
+    final uri = baseUrl.replace(queryParameters: {'BlockId': selectedBlockID});
 
     final response = await http.get(uri);
 
     if (response.statusCode == 200) {
       List<dynamic> jsonResponse = json.decode(response.body);
-      print(jsonResponse);
       List<Units> units =
           jsonResponse.map((unit) => Units.fromJson(unit)).toList();
 
       // Sorting the units based on Unit Number
       units.sort((a, b) => a.unitNumber.compareTo(b.unitNumber));
-      print(units);
       return units;
     } else {
       throw Exception(
@@ -176,7 +170,7 @@ class _MeterScreenState extends State<MeterScreen> {
                     Text(
                       'Property Name: ${_prop.name}',
                       style: const TextStyle(
-                          fontSize: 20.0,
+                          fontSize: 16.0,
                           color: Color.fromARGB(255, 166, 160, 55),
                           fontWeight: FontWeight.bold),
                     ),
@@ -208,8 +202,8 @@ class _MeterScreenState extends State<MeterScreen> {
                               onChanged: (selectedBlock) {
                                 setState(() {
                                   this.selectedBlock = selectedBlock;
-                                  selectedBlockID = selectedBlock
-                                      ?.blockId; // BlockId to be able to select units
+                                  selectedBlockID = selectedBlock!
+                                      .blockId; // BlockId to be able to select units
 
                                   // call method to fetch units when block is clicked
                                 });
@@ -263,93 +257,157 @@ class _MeterScreenState extends State<MeterScreen> {
                     left: 31.0, top: 16.0, bottom: 6.0, right: 18.0),
                 child: Container(
                   width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.7,
+                  height: MediaQuery.of(context).size.height * 0.6,
                   color: Colors.purple,
                   // Add code here:
-                  child: SingleChildScrollView(
-                    child: Wrap(direction: Axis.horizontal, children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.8 / 5,
-                        height: MediaQuery.of(context).size.height,
-                        color: Colors.blue,
-                        child: Column(
-                          children: <Widget>[
+                  child: RawScrollbar(
+                    thumbColor: Color.fromARGB(255, 166, 160, 55),
+                    radius: Radius.zero,
+                    thickness: 10,
+                    controller: _firstController,
+                    child: SingleChildScrollView(
+                      controller: _firstController,
+                      child: Wrap(direction: Axis.horizontal, children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8 / 5,
+                          height: MediaQuery.of(context).size.height,
+                          color: Colors.blue,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: double.infinity,
+                                height: MediaQuery.of(context).size.height,
+                                child: FutureBuilder<List<Units>>(
+                                  future: futureUnits,
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return const Center(
+                                        child: CircularProgressIndicator(
+                                            color: Color.fromARGB(
+                                                255, 166, 160, 55)),
+                                      );
+                                    } else if (snapshot.hasError) {
+                                      return Center(
+                                          child: Text('${snapshot.error}'));
+                                    } else {
+                                      List<Units> units = snapshot.data!;
+
+                                      return ListView.separated(
+                                          padding: const EdgeInsets.all(2.0),
+                                          itemBuilder:
+                                              (BuildContext context, index) {
+                                            Units unit = units[index];
+                                            print(unit.unitNumber);
+                                            return Text(
+                                                'Unit ${unit.unitNumber}');
+                                          },
+                                          separatorBuilder:
+                                              (BuildContext context,
+                                                      int index) =>
+                                                  const SizedBox(height: 8.0),
+                                          itemCount: units.length);
+                                    }
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8 / 5,
+                          height: MediaQuery.of(context).size.height,
+                          color: Colors.amber,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: double.infinity,
+                                height: MediaQuery.of(context).size.height,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: ((MediaQuery.of(context).size.width) +
+                                  (MediaQuery.of(context).size.width) * 0.4) /
+                              5,
+                          height: MediaQuery.of(context).size.height,
+                          color: Colors.green,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: double.infinity,
+                                height: MediaQuery.of(context).size.height,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8 / 5,
+                          height: MediaQuery.of(context).size.height,
+                          color: Colors.orange,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                width: double.infinity,
+                                height: MediaQuery.of(context).size.height,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.82 / 5,
+                          height: MediaQuery.of(context).size.height,
+                          color: Colors.red,
+                          child: Column(children: <Widget>[
                             Container(
                               width: double.infinity,
                               height: MediaQuery.of(context).size.height,
-                              child: FutureBuilder<List<Units>>(
-                                future: futureUnits,
-                                builder: (context, snapshot) {
-                                  if (snapshot.connectionState ==
-                                      ConnectionState.waiting) {
-                                    return const Center(
-                                      child: CircularProgressIndicator(
-                                          color: Color.fromARGB(
-                                              255, 166, 160, 55)),
-                                    );
-                                  } else if (snapshot.hasError) {
-                                    print(snapshot.error);
-                                    return Center(
-                                        child: Text('${snapshot.error}'));
-                                  } else {
-                                    List<Units> units = snapshot.data!;
-
-                                    return ListView.separated(
-                                        padding: const EdgeInsets.all(2.0),
-                                        itemBuilder:
-                                            (BuildContext context, index) {
-                                          Units unit = units[index];
-                                          print(unit.unitNumber);
-                                          return Text(
-                                              'Unit ${unit.unitNumber}');
-                                        },
-                                        separatorBuilder:
-                                            (BuildContext context, int index) =>
-                                                const SizedBox(height: 8.0),
-                                        itemCount: units.length);
-                                  }
-                                },
-                              ),
-                              //ListView.separated(
-                              //     padding: const EdgeInsets.all(2.0),
-                              //     itemBuilder:
-                              //         (BuildContext context, int index) {
-                              //       return Text(uni[index]);
-                              //     },
-                              //     separatorBuilder:
-                              //         (BuildContext context, int index) =>
-                              //             const SizedBox(
-                              //               height: 8.0,
-                              //             ),
-                              //     itemCount: uni.length),
                             ),
-                          ],
+                          ]),
                         ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.8 / 5,
-                        height: 40,
-                        color: Colors.amber,
-                      ),
-                      Container(
-                        width: ((MediaQuery.of(context).size.width) +
-                                (MediaQuery.of(context).size.width) * 0.4) /
-                            5,
-                        height: 40,
-                        color: Colors.green,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.8 / 5,
-                        height: 40,
-                        color: Colors.orange,
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.8 / 5,
-                        height: 40,
-                        color: Colors.red,
-                      ),
-                    ]),
+                      ]),
+                    ),
                   ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 31.0, top: 18.0, bottom: 1.0, right: 18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FxButton.medium(
+                      elevation: 1,
+                      borderRadiusAll: 0.0,
+                      backgroundColor: const Color.fromARGB(255, 207, 119, 40),
+                      onPressed: () {
+                        // Logic to Navigate back to Home Screen
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (_) => PropertyScreen()));
+                      },
+                      child: const Text(
+                        'Home',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16), // Double the font size
+                      ),
+                    ),
+                    FxButton.medium(
+                      elevation: 1,
+                      borderRadiusAll: 0.0,
+                      backgroundColor: const Color.fromARGB(255, 207, 119, 40),
+                      onPressed: () {
+                        // Logic to Post all recorded Meter readings back to the Server
+                      },
+                      child: const Text(
+                        'Submit',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16), // Double the font size
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
